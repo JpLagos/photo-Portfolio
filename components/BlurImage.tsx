906c187da680012e58abd3fc7e93bbfc3d67a7cd
Juan Lagos <juan.lagoshuerta@gmail.com>
2022-04-27 00:05:43 -0400

    type fixed

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -5,10 +5,11 @@ function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function BlurImage({ image }: { image: typeof Image }) {
+export default function BlurImage({ image }: { image:  Image }) {
   const [isLoading, setLoading] = useState(true);
+  console.log(image)
   return (
-    <a href={image.href} className="group">
+    <a href={image.href ? image.href : "loading..."} className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
@@ -25,7 +26,14 @@ export default function BlurImage({ image }: { image: typeof Image }) {
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.userName}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
   );
+}
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
 }
--- pages/photoGallery.tsx
+++ pages/photoGallery.tsx
@@ -18,9 +18,11 @@ export default function Gallery({ images }: { images: Image[] }) {
 }
 
 export async function getStaticProps() {
+ 
   const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SERVICE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SERVICE_KEY || ''
+
   )
   const { data } = await supabaseAdmin.from('images').select('*');
   return {
