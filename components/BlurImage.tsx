aff7578d217fea5805d022d1d66a3688284e73e6
Juan Lagos <juan.lagoshuerta@gmail.com>
2022-04-27 12:42:43 -0400

    BlurImage fixed

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -9,7 +9,7 @@ export default function BlurImage({ image }: { image:  Image }) {
   const [isLoading, setLoading] = useState(true);
   console.log(image)
   return (
-    <a href={image.href ? image.href : "loading..."} className="group">
+    <a href={image.href} className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
